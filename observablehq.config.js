d15e92ed4aae1d24278c7a3f8c368834f32260ce
aviralchawla <aviralchawlaviif@gmail.com>
2025-08-16 14:13:59 -0400

    before the main changes

--- observablehq.config.js
+++ observablehq.config.js
@@ -2,7 +2,7 @@
 export default {
   // The app’s title; used in the sidebar and webpage titles.
   title: "Aviral Chawla",
-  base: "/aviralchawla.github.io/",
+  // base: "/aviralchawla.github.io/",
 
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
