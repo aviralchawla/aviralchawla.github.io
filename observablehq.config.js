1e19cae48ff57b7c26ae611a790aed1502bd7eac
aviralchawla <aviralchawlaviif@gmail.com>
2025-08-16 16:23:45 -0400

    base

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,16 +1,18 @@
 export default {
 	title: "Aviral Chawla",
-	pages: [
-
-		{
-			name: "CV",
-			pages: [
-				{name: "CV", url: "https://www.aviralchawla.github.io/CV-Chawla.pdf"},
-			],
-		}
-	],
+	pages: [ {name: "CV", path:"./data/CV-Chawla.pdf"} ],
+	// pages: [
+	//
+	// 	{
+	// 		name: "CV",
+	// 		pages: [
+	// 			{name: "CV", url: "https://www.aviralchawla.github.io/CV-Chawla.pdf"},
+	// 		],
+	// 	}
+	// ],
 	head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
 	root: "src",
 	footer: "",
 	theme: "light",
+	pager: false
 };
